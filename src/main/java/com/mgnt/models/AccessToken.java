e1f0b1b756c491e95c2989435e1b723ca73b48d3
rn-santos227 <rn_santos227@yahoo.com>
2024-03-18 23:07:57 +0800

    added access token getters and setters

--- src/main/java/com/mgnt/models/AccessToken.java
+++ src/main/java/com/mgnt/models/AccessToken.java
@@ -19,4 +19,32 @@ public class AccessToken {
   @ManyToOne
   @JoinColumn(name = "user_id", referencedColumnName = "id")
   private User user;
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getToken() {
+    return token;
+  }
+
+  public void setToken(String token) {
+    this.token = token;
+  }
+  
+  public Date getExpiryDate() {
+    return expiryDate;
+  }
+
+  public void setExpiryDate(Date expiryDate) {
+    this.expiryDate = expiryDate;
+  }
+
+  public User getUser() {
+    return user;
+  }
+
+  public void setUser(User user) {
+    this.user = user;
+  }
 }
