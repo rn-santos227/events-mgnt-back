4e586b152087c45015739b872e0aa6fe4df81da5
rn-santos227 <rn_santos227@yahoo.com>
2024-03-18 21:56:59 +0800

    implemented dotenv in App.java

--- src/main/java/com/mgnt/App.java
+++ src/main/java/com/mgnt/App.java
@@ -1,13 +1,15 @@
 package com.mgnt;
 
-/**
- * Hello world!
- *
- */
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+        SpringApplication.run(App.class, args);
     }
 }
