f49c0374c0d1dc9bbdb2ebc391f1522cae2a2cc7
rn-santos227 <rn_santos227@yahoo.com>
2024-03-19 21:54:43 +0800

    created user service

---
+++ src/main/java/com/mgnt/services/UserService.java
@@ -0,0 +1,30 @@
+package com.mgnt.services;
+
+import com.mgnt.models.User;
+import com.mgnt.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService  {
+  private final UserRepository userRepository;
+  @Autowired
+  public UserService(UserRepository userRepository) {
+      this.userRepository = userRepository;
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    User user = userRepository.findByEmail(email)
+    .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+
+    return org.springframework.security.core.userdetails.User.builder()
+    .username(user.getEmail())
+    .password(user.getPassword())
+    .roles("USER")
+    .build();
+  }
+}
