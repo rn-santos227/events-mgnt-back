ad05843615db97336e566dbe934fc7d15e67d8cd
rn-santos227 <rn_santos227@yahoo.com>
2024-03-19 21:22:58 +0800

    implemented public test routes in security configuration

--- src/main/java/com/mgnt/config/SecurityConfig.java
+++ src/main/java/com/mgnt/config/SecurityConfig.java
@@ -2,11 +2,26 @@ package com.mgnt.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 
+
 @Configuration
-public class SecurityConfig {
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter  {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+      http
+      .authorizeRequests()
+      .antMatchers("/public/**")
+      .permitAll() 
+      .anyRequest()
+      .authenticated();
+    }
   @Bean
   public CsrfTokenRepository csrfTokenRepository() {
     return new CookieCsrfTokenRepository();
--- src/main/java/com/mgnt/controllers/TestController.java
+++ src/main/java/com/mgnt/controllers/TestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-  @GetMapping("/test")
+  @GetMapping("/public/test")
   public String test() {
     return "Hello, this is a test route!";
   }
