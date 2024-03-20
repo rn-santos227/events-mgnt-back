bcf4b037399afb3822b654b2e2d3ba58217d386e
rn-santos227 <rn_santos227@yahoo.com>
2024-03-20 22:05:35 +0800

    added packages in auth controller

--- src/main/java/com/mgnt/controllers/AuthController.java
+++ src/main/java/com/mgnt/controllers/AuthController.java
@@ -1,6 +1,22 @@
 package com.mgnt.controllers;
 
+import com.mgnt.models.AccessToken;
+import com.mgnt.models.User;
+import com.mgnt.services.TokenService;
+import com.mgnt.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class AuthController {
