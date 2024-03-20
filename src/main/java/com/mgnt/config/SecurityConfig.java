716779a9d4edb1a95cc9535b49495e66b7bde6bc
rn-santos227 <rn_santos227@yahoo.com>
2024-03-20 22:39:53 +0800

    update security configuration

--- src/main/java/com/mgnt/config/SecurityConfig.java
+++ src/main/java/com/mgnt/config/SecurityConfig.java
@@ -2,16 +2,27 @@ package com.mgnt.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
+
+import com.mgnt.services.UserService;
 
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter  {
+  private final UserService userService;
+  
+  public SecurityConfig(UserService userService) {
+    this.userService = userService;
+  }
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
@@ -23,8 +34,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter  {
     .csrf().csrfTokenRepository(csrfTokenRepository());
   }
 
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
+  }
+
   @Bean
   public CsrfTokenRepository csrfTokenRepository() {
     return new CookieCsrfTokenRepository();
+  }
+
+  @Bean
+  @Override
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
